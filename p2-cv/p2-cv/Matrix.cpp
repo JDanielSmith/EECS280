37eb9bbe951e0694ca9cf26c45ccd1e2845559e3
J. Daniel Smith <dan@jdanielsmith.me>
2023-01-24 22:20:44 -0500

    Matrix_fill_border

--- p2-cv/p2-cv/Matrix.cpp
+++ p2-cv/p2-cv/Matrix.cpp
@@ -4,17 +4,8 @@
 
 #include <cassert>
 #include <stdexcept>
-
-struct const_int_span final
-{
-    const int* data;
-    size_t size;
-};
-struct int_span final
-{
-    int* data;
-    size_t size;
-};
+#include <type_traits>
+#include <vector>
 
 // REQUIRES: mat points to a Matrix
 //           0 < width && width <= MAX_MATRIX_WIDTH
@@ -22,8 +13,29 @@ struct int_span final
 // MODIFIES: *mat
 // EFFECTS:  Initializes *mat as a Matrix with the given width and height.
 // NOTE:     Do NOT use new or delete here.
-static void Matrix_init(Matrix& mat, int width, int height)
+
+int* Matrix_data(Matrix* mat)
 {
+    if (mat == nullptr)
+    {
+        throw std::invalid_argument("mat");
+    }
+    return mat->data;
+}
+const int* Matrix_data(const Matrix* mat)
+{
+    if (mat == nullptr)
+    {
+        throw std::invalid_argument("mat");
+    }
+    return mat->data;
+}
+
+void Matrix_init(Matrix* mat, int width, int height) {
+    if (mat == nullptr)
+    {
+        throw std::invalid_argument("mat");
+    }
     if (!((0 < width) && (width <= MAX_MATRIX_WIDTH)))
     {
         throw std::invalid_argument("width");
@@ -32,15 +44,17 @@ static void Matrix_init(Matrix& mat, int width, int height)
     {
         throw std::invalid_argument("height");
     }
-    mat.width = width;
-    mat.height = height;
-}
-void Matrix_init(Matrix* mat, int width, int height) {
-    if (mat == nullptr)
+
+    mat->width = width;
+    mat->height = height;
+
+    constexpr auto size = std::extent<decltype(mat->data)>::value;
+    assert(size == (MAX_MATRIX_WIDTH * MAX_MATRIX_HEIGHT));
+    int* const data = Matrix_data(mat);
+    for (size_t i = 0; i < size; i++)
     {
-        throw std::invalid_argument("mat");
+       data[i] = -99999;
     }
-    Matrix_init(*mat, width, height);    
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -51,17 +65,21 @@ void Matrix_init(Matrix* mat, int width, int height) {
 //           Each element is followed by a space and each row is followed
 //           by a newline. This means there will be an "extra" space at
 //           the end of each line.
-static void Matrix_print(const Matrix& mat, std::ostream& os)
-{
-    os << Matrix_width(&mat) << " " << Matrix_height(&mat) << "\n";
-    assert(false); // TODO Replace with your implementation!
-}
 void Matrix_print(const Matrix* mat, std::ostream& os) {
     if (mat == nullptr)
     {
         throw std::invalid_argument("mat");
     }
-    Matrix_print(*mat, os);
+
+    os << Matrix_width(mat) << " " << Matrix_height(mat) << "\n";
+    for (int r = 0; r < Matrix_height(mat); r++)
+    {
+        for (int c = 0; c < Matrix_width(mat); c++)
+        {
+            os << *(Matrix_at(mat, r, c)) << " ";
+        }
+        os << "\n";
+    }
 }
 
 // REQUIRES: mat points to an valid Matrix
@@ -87,33 +105,51 @@ int Matrix_height(const Matrix* mat) {
 // REQUIRES: mat points to a valid Matrix
 //           ptr points to an element in the Matrix
 // EFFECTS:  Returns the row of the element pointed to by ptr.
-static int Matrix_row(const Matrix& mat, const int* ptr)
+static int size(const Matrix* mat)
 {
-    assert(false); // TODO Replace with your implementation!
-    return -1;
+    return Matrix_width(mat) * Matrix_height(mat);
 }
-int Matrix_row(const Matrix* mat, const int* ptr) {
+
+static void validate_ptr(const Matrix* mat, const int* ptr)
+{
     if (mat == nullptr)
     {
         throw std::invalid_argument("mat");
     }
-    return Matrix_row(*mat, ptr);
+    if (ptr == nullptr)
+    {
+        throw std::invalid_argument("ptr");
+    }
+
+    const int* const p = Matrix_data(mat);
+    if (ptr < p)
+    {
+        throw std::invalid_argument("ptr");
+    }
+    const auto sz = size(mat);
+    if (ptr >= p + sz)
+    {
+        throw std::invalid_argument("ptr");
+    }
+}
+
+int Matrix_row(const Matrix* mat, const int* ptr) {
+    validate_ptr(mat, ptr);
+
+    const auto diff = ptr - Matrix_data(mat);
+    const auto retval = diff / Matrix_width(mat);
+    return static_cast<int>(retval);
 }
 
 // REQUIRES: mat points to a valid Matrix
 //           ptr point to an element in the Matrix
 // EFFECTS:  Returns the column of the element pointed to by ptr.
-static int Matrix_column(const Matrix& mat, const int* ptr)
-{
-    assert(false); // TODO Replace with your implementation!
-    return -1;
-}
 int Matrix_column(const Matrix* mat, const int* ptr) {
-    if (mat == nullptr)
-    {
-        throw std::invalid_argument("mat");
-    }
-    return Matrix_column(*mat, ptr);
+    validate_ptr(mat, ptr);
+
+    const auto diff = ptr - Matrix_data(mat);
+    const auto retval = diff % Matrix_width(mat);
+    return static_cast<int>(retval);
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -124,21 +160,37 @@ int Matrix_column(const Matrix* mat, const int* ptr) {
 //            element in the Matrix.)
 // EFFECTS:  Returns a pointer to the element in the Matrix
 //           at the given row and column.
-int* Matrix_at(Matrix* mat, int row, int column) {
+static void validate_row(const Matrix* mat, int row)
+{
     if (mat == nullptr)
     {
         throw std::invalid_argument("mat");
     }
-    if (! ((0 <= row) && (row < Matrix_height(mat))) )
+    if (!((0 <= row) && (row < Matrix_height(mat))))
     {
         throw std::invalid_argument("row");
     }
-    if (! ((0 <= column) && (column <= Matrix_width(mat))) )
+}
+static void validate_column(const Matrix* mat, int column)
+{
+    if (mat == nullptr)
+    {
+        throw std::invalid_argument("mat");
+    }
+    if (!((0 <= column) && (column <= Matrix_width(mat))))
     {
         throw std::invalid_argument("column");
     }
-    assert(false); // TODO Replace with your implementation!
-    return nullptr;
+}
+
+static void validate_row_column(const Matrix* mat, int row, int column)
+{
+    validate_row(mat, row);
+    validate_column(mat, column);
+}
+int* Matrix_at(Matrix* mat, int row, int column) {
+    validate_row_column(mat, row, column);
+    return Matrix_data(mat) + (row * Matrix_width(mat) + column);
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -148,31 +200,32 @@ int* Matrix_at(Matrix* mat, int row, int column) {
 // EFFECTS:  Returns a pointer-to-const to the element in
 //           the Matrix at the given row and column.
 const int* Matrix_at(const Matrix* mat, int row, int column) {
-    if (mat == nullptr)
-    {
-        throw std::invalid_argument("mat");
-    }
-    if (! ((0 <= row) && (row < Matrix_height(mat))) )
-    {
-        throw std::invalid_argument("row");
-    }
-    if (! ((0 <= column) && (column <= Matrix_width(mat))) )
-    {
-        throw std::invalid_argument("column");
-    }
-    assert(false); // TODO Replace with your implementation!
-    return nullptr;
+    validate_row_column(mat, row, column);
+    return Matrix_data(mat) + (row * Matrix_width(mat) + column);
 }
 
 // REQUIRES: mat points to a valid Matrix
 // MODIFIES: *mat
 // EFFECTS:  Sets each element of the Matrix to the given value.
+static void fill_row(Matrix* mat, int row, int value)
+{
+    validate_row(mat, row);
+    for (int c = 0; c < Matrix_width(mat); c++)
+    {
+        *(Matrix_at(mat, row, c)) = value;
+    }
+}
+
 void Matrix_fill(Matrix* mat, int value) {
     if (mat == nullptr)
     {
         throw std::invalid_argument("mat");
     }
-    assert(false); // TODO Replace with your implementation!
+
+    for (int r = 0; r < Matrix_height(mat); r++)
+    {
+        fill_row(mat, r, value);
+    }
 }
 
 // REQUIRES: mat points to a valid Matrix
@@ -180,12 +233,26 @@ void Matrix_fill(Matrix* mat, int value) {
 // EFFECTS:  Sets each element on the border of the Matrix to
 //           the given value. These are all elements in the first/last
 //           row or the first/last column.
+static void fill_column(Matrix* mat, int column, int value)
+{
+    validate_column(mat, column);
+    for (int r = 0; r < Matrix_height(mat); r++)
+    {
+        *(Matrix_at(mat, r, column)) = value;
+    }
+}
+
 void Matrix_fill_border(Matrix* mat, int value) {
     if (mat == nullptr)
     {
         throw std::invalid_argument("mat");
     }
-    assert(false); // TODO Replace with your implementation!
+
+    fill_row(mat, 0, value);
+    fill_row(mat, Matrix_height(mat)-1, value);
+
+    fill_column(mat, 0, value);
+    fill_column(mat, Matrix_width(mat)-1, value);
 }
 
 // REQUIRES: mat points to a valid Matrix
--- p2-cv/p2-cv/Matrix_tests.cpp
+++ p2-cv/p2-cv/Matrix_tests.cpp
@@ -1,11 +1,59 @@
 // Project UID af1f95f547e44c8ea88730dfb185559d
 
+#include <vector>
+
 #include "Matrix.h"
 #include "Matrix_test_helpers.h"
 #include "unit_test_framework.h"
 
 using namespace std;
 
+
+// This is the public Matrix test for which the autograder gives feedback.
+// It only tests VERY FEW of the expected behaviors of the Matrix module.
+// It will only really tell you if your code compiles and you remembered to
+// write the functions. It is not to be trusted. It tells the truth, but not
+// the whole truth. It might put you in a blender. You get the point.
+// You must write your own comprehensive unit tests in Matrix_tests.cpp!
+
+TEST(test_matrix_basic) {
+    std::vector<Matrix> v(1);
+    auto mat = &(v.front());
+    Matrix_init(mat, 5, 5);
+
+    ASSERT_EQUAL(Matrix_width(mat), 5);
+    ASSERT_EQUAL(Matrix_height(mat), 5);
+
+    Matrix_fill(mat, 0);
+
+    int* ptr = Matrix_at(mat, 2, 3);
+    ASSERT_EQUAL(Matrix_row(mat, ptr), 2);
+    ASSERT_EQUAL(Matrix_column(mat, ptr), 3);
+    ASSERT_EQUAL(*ptr, 0);
+    *ptr = 42;
+
+    const int* cptr = Matrix_at(mat, 2, 3);
+    ASSERT_EQUAL(*cptr, 42);
+
+    Matrix_fill_border(mat, 2);
+    ASSERT_EQUAL(*Matrix_at(mat, 0, 0), 2);
+
+    //ASSERT_EQUAL(Matrix_max(mat), 42);
+}
+
+TEST(test_matrix_print) {
+    std::vector<Matrix> v(1);
+    auto mat = &(v.front());
+    Matrix_init(mat, 1, 1);
+
+    *Matrix_at(mat, 0, 0) = 42;
+    ostringstream expected;
+    expected << "1 1\n"
+        << "42 \n";
+    ostringstream actual;
+    Matrix_print(mat, actual);
+    ASSERT_EQUAL(expected.str(), actual.str());
+}
 
 // Here's a free test for you! Model yours after this one.
 // Test functions have no interface and thus no RMEs, but
@@ -14,25 +62,83 @@ using namespace std;
 // Fills a 3x5 Matrix with a value and checks
 // that Matrix_at returns that value for each element.
 TEST(test_fill_basic) {
-  Matrix *mat = new Matrix; // create a Matrix in dynamic memory
+    std::vector<Matrix> v(1);
+    auto mat = &(v.front());
 
-  const int width = 3;
-  const int height = 5;
-  const int value = 42;
-  Matrix_init(mat, 3, 5);
-  Matrix_fill(mat, value);
+    const int width = 3;
+    const int height = 5;
+    const int value = 42;
+    Matrix_init(mat, 3, 5);
+    Matrix_fill(mat, value);
 
-  for(int r = 0; r < height; ++r){
-    for(int c = 0; c < width; ++c){
-      ASSERT_EQUAL(*Matrix_at(mat, r, c), value);
+    for (int r = 0; r < height; ++r) {
+        for (int c = 0; c < width; ++c) {
+            ASSERT_EQUAL(*Matrix_at(mat, r, c), value);
+        }
     }
-  }
-
-  delete mat; // delete the Matrix
 }
 
 // ADD YOUR TESTS HERE
 // You are encouraged to use any functions from Matrix_test_helpers.h as needed.
+TEST(test_Matrix_row_column) {
+    std::vector<Matrix> v(1);
+    auto mat_ = &(v.front());
+    Matrix_init(mat_, 3, 4);
+    const auto mat = mat_;
+
+    for (int r = 0; r < Matrix_height(mat); r++)
+    {
+        for (int c = 0; c < Matrix_width(mat); c++)
+        {
+            const int* const ptr = Matrix_at(mat, r, c);
+            ASSERT_EQUAL(Matrix_row(mat, ptr), r);
+            ASSERT_EQUAL(Matrix_column(mat, ptr), c);
+        }
+    }
+}
+
+TEST(test_Matrix_fill_border) {
+    std::vector<Matrix> v(1);
+    auto mat = &(v.front());
+    Matrix_init(mat, 8, 12);
+
+    Matrix_fill(mat, 1);
+
+    Matrix_fill_border(mat, 0);
+
+    constexpr auto first_row = 0;
+    constexpr auto first_col = 0;
+    const auto last_row = Matrix_height(mat) - 1;
+    const auto last_col = Matrix_width(mat) - 1;
+
+    // first and last row
+    ASSERT_EQUAL(*Matrix_at(mat, first_row, first_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, first_row, 1), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, first_row, 3), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, first_row, last_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, last_row, first_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, last_row, 1), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, last_row, 3), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, last_row, last_col), 0);
+
+    // first and last column
+    ASSERT_EQUAL(*Matrix_at(mat, first_row, first_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, 1, first_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, 3, first_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, last_row, first_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, first_row, last_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, 1, last_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, 3, last_col), 0);
+    ASSERT_EQUAL(*Matrix_at(mat, last_row, last_col), 0);
+
+    // diagonals
+    ASSERT_EQUAL(*Matrix_at(mat, 1, 1), 1);
+    ASSERT_EQUAL(*Matrix_at(mat, 2, 2), 1);
+    ASSERT_EQUAL(*Matrix_at(mat, 3, 3), 1);
+    ASSERT_EQUAL(*Matrix_at(mat, 1, 3), 1);
+    ASSERT_EQUAL(*Matrix_at(mat, 2, 2), 1);
+    ASSERT_EQUAL(*Matrix_at(mat, 3, 1), 1);
+}
 
 
 // NOTE: The unit test framework tutorial in Lab 2 originally
